584761214850176e383e0cd55014c18c3f8962b0
HP\Ravishankar <ravishankar2106@gmail.com>
2017-01-25 17:44:44 +0530

    Properties file reading changes

--- src/main/java/com/bind/ptw/be/initializers/HibernateConfiguration.java
+++ src/main/java/com/bind/ptw/be/initializers/HibernateConfiguration.java
@@ -6,10 +6,12 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
@@ -20,37 +22,43 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan("com.bind.ptw.be.entities")
 @PropertySource(value = {"classpath:application.properties"})
+@PropertySource("file:${prop.path}/app.properties")
 public class HibernateConfiguration {
-  @Bean
-  public HibernateTemplate hibernateTemplate() {
-    return new HibernateTemplate(sessionFactory());
-  }
+	
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public HibernateTemplate hibernateTemplate() {
+		return new HibernateTemplate(sessionFactory());
+	}
 
-  @Bean
-  public SessionFactory sessionFactory() {
+	@Bean
+	public SessionFactory sessionFactory() {
 
-    LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-    sessionFactoryBean.setDataSource(getDataSource());
-    sessionFactoryBean.setPackagesToScan("com.bind.ptw.be.entities");
-    Properties hibernateProperties = new Properties();
-    hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-    hibernateProperties.put("hibernate.show_sql", true);
-    hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
-    sessionFactoryBean.setHibernateProperties(hibernateProperties);
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(getDataSource());
+		sessionFactoryBean.setPackagesToScan("com.bind.ptw.be.entities");
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		hibernateProperties.put("hibernate.show_sql", true);
+		hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties);
 
-    // return (SessionFactory) sessionFactoryBean.get;
+		// return (SessionFactory) sessionFactoryBean.get;
 
-    return new LocalSessionFactoryBuilder(getDataSource()).scanPackages("com.bind.ptw.be.entities").addProperties(hibernateProperties)
-        .buildSessionFactory();
-  }
+		return new LocalSessionFactoryBuilder(getDataSource()).scanPackages("com.bind.ptw.be.entities").addProperties(hibernateProperties)
+				.buildSessionFactory();
+	}
 
   @Bean
   public DataSource getDataSource() {
     BasicDataSource dataSource = new BasicDataSource();
     dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/ptwdev");
-    dataSource.setUsername("root");
-    dataSource.setPassword("root");
+    dataSource.setUrl("jdbc:mysql://:"+ env.getProperty("db.port")+"/"+env.getProperty("db.dbname"));;
+    dataSource.setUsername(env.getProperty("db.username"));
+    dataSource.setPassword(env.getProperty("db.password"));
 
 
     return dataSource;
